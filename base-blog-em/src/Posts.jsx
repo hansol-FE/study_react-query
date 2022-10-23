e206010964ba2670376a40c45d66be3921fb327a
hansol <dundunki@naver.com>
2022-10-23 15:38:36 +0900

    solved code quiz

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -16,7 +16,7 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const { data,isLoading } = useQuery("posts", fetchPosts);
+  const { data,isLoading } = useQuery("posts", fetchPosts,{staleTime:2000});
   if(isLoading){
     return <div>loading...</div>
   }
